e65ad2b59d7b781af1c950f8aafec7e0d1cbc5ab
Mahfuzur Rahman <mahfuz@student.sust.edu>
2018-03-11 17:55:01 +0600

    * .gitignore: Initial commit.
    * m.AppLogger.sln:
    * .nuspec:
    * icon.png:
    * Logger.cs:
    * m.AppLogger.csproj:

---
+++ AppLogger/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace m.AppLogger
+{
+    public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss - ") + text);
+        }
+    }
+}
